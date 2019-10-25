91f269614462273187b7f3bdfc130d25434f5b2d
CGMN <hadesis@gmail.com>
2019-10-25 12:31:00 -0300

    Update actualizaciones_estado.py
    solo falta probar en la base real

--- actualizaciones_estado.py
+++ actualizaciones_estado.py
@@ -4,20 +4,33 @@ try:
     connection = psycopg2.connect(user="admin", password="admin",
                               host="localhost", port="5432", database="Gestion_RRHH")
     cursor = connection.cursor()
-    # Print PostgreSQL Connection properties
-    #print (connection.get_dsn_parameters(),"\n")
     print ("Se ha conectado satisfactoriamente a la base de datos","\n")
+
     # actualizar estado
-
-    #entrada de mes desde el que se toma razon
-
     print ("Escriba el mes de inicio de la toma de razón, en minusculas")
+    meses_anio=['enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre']
+
     mes_inicio=input()
 
-    cursor.execute("UPDATE presup_est SET anio = '9999' WHERE anio = '2018' AND mes IN('octubre','noviembre','diciembre')")
-    connection.commit()
-    print ("Se han realizado los cambios")
+    meses_considerados=[]
 
+    ind_inicio=meses_anio.index(mes_inicio)
+
+    i=ind_inicio
+    while i<12:
+        meses_considerados.append(meses_anio[i])
+        i+=1
+
+    meses=tuple(meses_considerados)
+
+    if mes_inicio in meses_anio:
+        sql=cursor.mogrify("UPDATE presup_est SET anio = '9999' WHERE anio = '2018' AND mes IN %s;",(meses,))
+        cursor.execute(sql)
+        conteo=cursor.rowcount
+        connection.commit()
+        print ("\nSe han modificado:",conteo,"filas")
+    else:
+        print ("\nMes no corresponde")
 
 except (Exception, psycopg2.Error) as error:
     print ("Error while connecting to PostgreSQL", error)
@@ -26,4 +39,4 @@ finally:
     if(connection):
         cursor.close()
         connection.close()
-        print("Conexión finalizada con PostgreSQL")
+        print("\nConexión finalizada con PostgreSQL")
